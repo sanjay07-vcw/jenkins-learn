029cafe51a08361f421bac281f9e7d01a12d9819
Sanjay <karthikeyanvelu777@gmail.com>
2025-09-19 19:44:22 +0530

    qd

--- Add_test.go
+++ Add_test.go
@@ -9,3 +9,27 @@ func TestAdd(t *testing.T) {
 	}
 	// t.Fatal() will stop the test immediately if it fails.
 }
+func TestSub(t *testing.T) {
+	result := 4 - 1
+	if result != 3 {
+		t.Errorf("Expected 3, got %d", result)
+	}
+}
+func TestMul(t *testing.T) {
+	result := 6 * 7
+	if result != 42 {
+		t.Errorf("Expected 42, got %d", result)
+	}
+}
+func TestDiv(t *testing.T) {
+	result := 8 / 2
+	if result != 4 {
+		t.Errorf("Expected 4, got %d", result)
+	}
+}
+func TestMod(t *testing.T) {
+	result := 9 % 4
+	if result != 1 {
+		t.Errorf("Expected 1, got %d", result)
+	}
+}
--- main.go
+++ main.go
@@ -2,10 +2,34 @@ package main
 
 func main() {
 	Add(2, 3)
+	Sub(2, 3)
+	Mul(2, 3)
+	Div(2, 3)
+	Mod(2, 3)
 
 }
 
 func Add(a, b int) {
 	var c = a + b
+	println(c)
+}
+
+func Sub(a, b int) {
+	var c = a - b
+	println(c)
+}
+
+func Mul(a, b int) {
+	var c = a * b
+	println(c)
+}
+
+func Div(a, b int) {
+	var c = a / b
+	println(c)
+}
+
+func Mod(a, b int) {
+	var c = a % b
 	println(c)
 }
